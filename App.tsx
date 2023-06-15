a85b79cf198819eb881741a0da4322091ffc1262
Thierry Ravelonjanahary <t.ravelonjanahary@gmail.com>
2023-06-15 16:58:22 +0200

    init application

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Platform, StatusBar, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={'dark-content'} />
+      <Text style={styles.title}>E2E testing with detox on {Platform.OS}</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#000',
+  },
+});
