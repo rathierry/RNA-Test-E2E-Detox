b45b7ffb3570a6f9a11d8070f4c2780011c1d73d
Thierry Ravelonjanahary <t.ravelonjanahary@gmail.com>
2023-06-16 18:05:56 +0200

    (test) improve tests

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -5,24 +5,29 @@ describe('Defaut E2E testing', () => {
     await device.launchApp();
   });
 
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
+  // FIXME: It is a good idea to start every test from a fresh state, since the preceding ones might leave your application in an unpredictable state if they fail.
+  // beforeEach(async () => {
+  //   await device.reloadReactNative();
+  // });
 
   it('should have title screen', async () => {
     const titleID = element(by.id('title'));
     await expect(titleID).toBeVisible();
   });
 
-  // TODO: improve test
-  // it('should show hello screen after tap', async () => {
-  //   await element(by.id('hello_button')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
+  const inputText = 'Starter test';
 
-  // TODO: improve test
-  // it('should show world screen after tap', async () => {
-  //   await element(by.id('world_button')).tap();
-  //   await expect(element(by.text('World!!!'))).toBeVisible();
-  // });
+  it('should type text', async () => {
+    const input = element(by.id('input'));
+    await input.typeText(inputText);
+  });
+
+  it('should tap on the press me button', async () => {
+    await element(by.id('button')).tap();
+  });
+
+  it('should have alert message with typed text', async () => {
+    await expect(element(by.text(inputText)).atIndex(1)).toBeVisible();
+    await element(by.text('OK')).tap();
+  });
 });
