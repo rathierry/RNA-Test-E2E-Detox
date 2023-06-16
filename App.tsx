b05561b43a7141f861f9f34079191fb053fae595
Thierry Ravelonjanahary <t.ravelonjanahary@gmail.com>
2023-06-16 18:05:09 +0200

    (app) add form

--- App.tsx
+++ App.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
-import {Platform, StatusBar, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Alert,
+  Platform,
+  Pressable,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 
 const App = () => {
+  const [input, setInput] = useState('');
+
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={'dark-content'} />
-      <Text testID="title" style={styles.title}>
-        E2E testing with detox on {Platform.OS}
-      </Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <StatusBar barStyle={'dark-content'} />
+
+        <Text testID="title" style={styles.title}>
+          E2E testing with detox on {Platform.OS}
+        </Text>
+
+        <View style={styles.spacer} />
+
+        <TextInput
+          testID="input"
+          placeholder="Your text"
+          value={input}
+          onChangeText={setInput}
+          style={styles.textInput}
+        />
+
+        <View style={styles.spacer} />
+
+        <Pressable
+          testID="button"
+          style={styles.buttonContainer}
+          onPress={() => Alert.alert('E2E Test', input)}>
+          <Text style={styles.buttonText}>Press me</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -17,11 +51,42 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#F1F3FF',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 8,
   },
   title: {
     color: '#000',
+    fontWeight: '700',
+  },
+  buttonContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 0.5,
+    borderColor: '#5B77FF',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    width: '100%',
+  },
+  buttonText: {
+    color: '#5B77FF',
+    fontSize: 16,
+    fontWeight: '500',
+    alignSelf: 'center',
+  },
+  textInput: {
+    borderWidth: 0.5,
+    borderColor: '#5B77FF',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    width: '100%',
+  },
+  spacer: {
+    marginVertical: 4,
   },
 });
